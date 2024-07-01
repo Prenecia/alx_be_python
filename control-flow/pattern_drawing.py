325dcf414fd3590a7e76cdf708f745f411509a9d
Prenecia <michaelaprenecia0@gmail.com>
2024-07-01 05:46:12 -0700

    update

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-# Prompt user for pattern size
+# Prompt the user for the size of the pattern
 size = int(input("Enter the size of the pattern: "))
 
-# Draw the pattern
-for _ in range(size):
-    for _ in range(size):
+# Draw the pattern using nested loops
+i = 0
+while i < size:
+    for j in range(size):
         print("*", end="")
     print()
+    i += 1
